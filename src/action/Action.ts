b618493d94b8dfed431222cddb5075c0dace0e7c
Daejung Kim <playerdecuple@gmail.com>
2023-10-31 19:25:06 +0900

    Complete Action object

--- src/action/Action.ts
+++ src/action/Action.ts
@@ -1,25 +1,102 @@
 import Deskot from "../Deskot";
+import Animation from "../animation/Animation";
+import Script from "../script/Script";
+
 import LateInit from "../type/Lateinit";
+
 
 
 abstract class Action {
     
     private deskot: LateInit<Deskot>;
 
-    private animations: Array<any>;
+    private animations: Array<Animation>;
 
-    private param: any;
+    private params: any;
+
+
+    private startTime: number = -1;
 
     
     // TODO: Change type of 'animations' local variable.
-    constructor(animations: Array<any> = [], param: any = {}) {
+    constructor(animations: Array<Animation> = [], params: any = {}) {
         this.animations = animations;
-        this.param = param;
+        this.params = params;
     }
 
 
     init(deskot: Deskot) {
         this.deskot = deskot;
+        this.setTime();
+
+        this.params.deskot = deskot;
+        this.params.action = this;
+
+        for (const param of Object.values(this.params)) {
+            if (param instanceof Script) {
+                param.init();
+            }
+        }
+        for (const animation of this.animations) {
+            animation.init();
+        }
+    }
+
+
+    next() {
+        this.initFrame();
+        this.tick();
+    }
+
+
+    initFrame() {
+        for (const param of Object.values(this.params)) {
+            if (param instanceof Script) {
+                param.initFrame();
+            }
+        }
+        for (const animation of this.animations) {
+            animation.initFrame();
+        }
+    }
+    
+
+    toString() {
+        return `${this.constructor.name}@Action (Name: ${this.params.name ?? "undefined"})`;
+    }
+
+
+    protected setTime(time: number = 0) {
+        this.startTime = this.deskot!!.time - time;
+    }
+    
+    protected getTime() {
+        return this.deskot!!.time - this.startTime;
+    }
+
+
+    protected tick() {}
+
+
+    hasNext() {
+        const isEffective = this.isEffective();
+        const isRunning = this.getTime() < this.getDuration();
+
+        return isEffective && isRunning;
+    }
+
+    
+    isEffective(): boolean {
+        // this.params.condition instanceof Script
+        return this.params.condition?.get(this.params) ?? true;
+    }
+
+    isDraggable(): boolean {
+        return this.params.draggable as boolean ?? true;
+    }
+
+    getDuration(): number {
+        return this.params.duration as number ?? Infinity;
     }
 
 }
