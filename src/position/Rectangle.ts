bbaeba4d10cd8105774826b80b3533c02b325489
Daejung Kim <playerdecuple@gmail.com>
2023-10-31 19:26:50 +0900

    Create Rectangle.ts

---
+++ src/position/Rectangle.ts
@@ -0,0 +1,22 @@
+class Rectangle {
+
+    x: number;
+
+    y: number;
+
+    width: number;
+
+    height: number;
+
+
+    constructor(x: number, y: number, width: number, height: number) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+}
+
+
+export default Rectangle;
