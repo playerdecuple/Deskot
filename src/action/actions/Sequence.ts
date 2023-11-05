469de65e105478ebd730a9eef57194d087e10748
Daejung Kim <playerdecuple@gmail.com>
2023-11-05 11:03:09 +0900

    Add loop count

--- src/action/actions/Sequence.ts
+++ src/action/actions/Sequence.ts
@@ -5,6 +5,9 @@ import ComplexAction from "../ComplexAction";
 
 class Sequence extends ComplexAction {
 
+    private sequenceCount: number = 1;
+
+
     constructor(params: any, actions: Array<Action>) {
         super(params, ...actions);
     }
@@ -12,7 +15,8 @@ class Sequence extends ComplexAction {
 
     override hasNext() {
         this.seek();
-        return super.hasNext();
+        return this.sequenceCount <= this.maximumSequenceCount
+            && super.hasNext();
     }
 
     
@@ -21,6 +25,10 @@ class Sequence extends ComplexAction {
     }
 
     override set currentActionIndex(index: number) {
+        if (index >= this.actions.length) {
+            this.sequenceCount++;
+        }
+
         super.currentActionIndex = this.isLoop 
             ? index % this.actions.length 
             : index;
@@ -28,7 +36,14 @@ class Sequence extends ComplexAction {
 
     
     get isLoop() {
-        return super.params?.loop === "true";
+        return super.params?.loop === "true" || super.params?.loop instanceof Number;
+    }
+
+    get maximumSequenceCount() {
+        if (super.params?.loop instanceof Number) {
+            return super.params?.loop;
+        }
+        return 1;
     }
 
 }
