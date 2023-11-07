46fd04ccd35c106cf9d8bbca28bf59529e358408
Daejung Kim <playerdecuple@gmail.com>
2023-11-08 04:46:59 +0900

    Remove menu bar(in Windows)

--- src/window/WindowManager.ts
+++ src/window/WindowManager.ts
@@ -30,6 +30,7 @@ class WindowManager {
 
     private initPage() {
         const { window } = this;
+        window.setMenu(null);
         window.loadFile(path.resolve(Main.path, "./public/index.html"));
         // window.webContents.openDevTools();
     }
