144b6bb5da1a82e67161348565c8997c05c614c5
Daejung Kim <playerdecuple@gmail.com>
2023-11-07 17:19:05 +0900

    Add behavior executor function

--- src/window/WindowManager.ts
+++ src/window/WindowManager.ts
@@ -6,6 +6,7 @@ import Sound from "../sound/Sound";
 import Main from "../../main";
 import ImagePair from "../image/ImagePair";
 import Deskot from "../Deskot";
+import Behavior from "../behavior/Behavior";
 
 
 
@@ -13,7 +14,7 @@ class WindowManager {
 
     readonly deskot: Deskot;
 
-    readonly window: BrowserWindow;
+    window: BrowserWindow;
 
     
     private image: Nullable<ImagePair>;
@@ -30,12 +31,43 @@ class WindowManager {
     private initPage() {
         const { window } = this;
         window.loadFile(path.resolve(Main.path, "./public/index.html"));
+        window.webContents.openDevTools();
     }
 
     
     private initEventHandler() {
-        ipcMain.on('draw-response', (e, timeline) => {
-            // console.log(timeline);
+        ipcMain.on('draw-fail', (_) => {
+            this.deskot.log(`Auto-reloading Window...`);
+        });
+
+        ipcMain.on('execute-behavior', async (e, behaviorName) => {
+            if (!this.deskot.behaviorFactories.has(behaviorName)) {
+                e.reply('execute-behavior/failed', `Behavior '${behaviorName}' not found.`);
+            }
+
+            const behaviorFactory = this.deskot.behaviorFactories.get(behaviorName);
+
+            try {
+                const behavior = await behaviorFactory?.build();
+                this.deskot.setBehavior(behavior!);
+            } catch (ex) {
+                e.reply('execute-behavior/failed', `Failed to execute the behavior.\n\n${ex}`);
+            }
+        });
+
+        ipcMain.on('do-action', async (e, actionName) => {
+            if (!this.deskot.actionFactories.has(actionName)) {
+                e.reply('do-action/failed', `Action '${actionName}' not found.`);
+            }
+
+            const actionFactory = this.deskot.actionFactories.get(actionName);
+
+            try {
+                const action = await actionFactory?.build({});
+                this.deskot.setBehavior(new Behavior("UserRequestBehavior", action!, true));
+            } catch (ex) {
+                e.reply('do-action/failed', `Failed to execute the action.\n\n${ex}`);
+            }
         });
     }
 
